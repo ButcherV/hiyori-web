e75d90702eb333b2a00094adf20bb5e02b68860e
butherv <butchervv@gmail.com>
2025-12-14 13:42:26 +0800

    Init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>日和 Hiyori</h1>
+        <p>环境搭建完成 (No Tailwind)</p>
+      </header>
+      
+      <main>
+        <div className="card-demo">
+          <span>🇯🇵 + 🍎</span>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
