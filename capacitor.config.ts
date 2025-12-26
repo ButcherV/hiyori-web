b627fc14934fa46d4e5384ceebddf9fc29dbc5b7
butherv <butchervv@gmail.com>
2025-12-27 06:22:51 +0800

    IOS update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'hiyori-web',
-  webDir: 'dist'
+  webDir: 'dist',
 };
 
 export default config;
